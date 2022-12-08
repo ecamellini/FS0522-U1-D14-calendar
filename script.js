d19188bdba649a022632986975c71601920acb73
Eric Camellini <eric.camellini@gmail.com>
2022-12-08 11:11:40 +0100

    Implemented the day selection

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function onDayClick(event) {
+  // ^ ^  This is an event listener, we know it will receive as parameter the event data object
+
+  // We remove the class selected from the previously selected day
+
+  let previouslySelectedDay = document.querySelector(".selected"); // Get the first node matching the .selected selector
+  // SAME AS DOING THE FOLLOWING:
+  // let previouslySelectedDay = document.getElementsByClassName('selected')[0]
+  if (previouslySelectedDay !== null) {
+    previouslySelectedDay.classList.remove("selected");
+  }
+
+  // We clicked a day!
+  // How do we know which is the day we clicked?
+  let clickedDayNode = event.target; // The tag that triggered the event.
+  clickedDayNode.classList.add("selected");
+}
+
 function onLoadActions() {
   // The function that will be called right after the page loads correctly.
   // We do here everything we want to do as soon as the page loads.
@@ -13,6 +31,7 @@ function onLoadActions() {
     // 2) We customize it as needed
     dayNode.classList.add("day");
     dayNode.innerText = day;
+    dayNode.addEventListener("click", onDayClick); // Without ()
 
     // 3) Append it / add it somewhere in the DOM
     document.getElementById("days-container").appendChild(dayNode);
