f5072c6f395edb51ec84532777355f25ad7c12d6
Eric Camellini <eric.camellini@gmail.com>
2022-12-08 10:35:28 +0100

    Displaying days

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Hello!");
+function onLoadActions() {
+  // The function that will be called right after the page loads correctly.
+  // We do here everything we want to do as soon as the page loads.
+
+  // 1) We visualize the days.
+  // ASSUMPTION: let's display 30 days, always.
+  // TODO actually render the number of days of the current month.
+
+  for (let day = 1; day <= 30; day++) {
+    // 1) We create a div for the day
+    let dayNode = document.createElement("div");
+
+    // 2) We customize it as needed
+    dayNode.classList.add("day");
+    dayNode.innerText = day;
+
+    // 3) Append it / add it somewhere in the DOM
+    document.getElementById("days-container").appendChild(dayNode);
+  }
+}
+
+window.onload = onLoadActions;
